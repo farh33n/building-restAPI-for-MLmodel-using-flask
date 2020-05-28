a29702643d1de0f6f5a5cb0ad8e7a87b642798d8
farheen <sy3dafarheen@gmail.com>
2020-05-28 16:38:19 +0500

    Update app.py

--- app.py
+++ app.py
@@ -8,21 +8,20 @@ app = Flask(__name__)
 reg_model = Model()
 
 model_path = 'model/reg_model.pkl'
+# load model
 with open(model_path, 'rb') as f:
     reg_model.reg = joblib.load(f)
 
 @app.route('/get_prediction', methods=['GET'])
 def get_prediction():
     data = request.args.getlist('var[]', type=float)
-    type1 = type(data)
     data = np.array(data, dtype=np.float32)
-    type2 = type(data)
     data = np.reshape(data, (1, -1))
     prediction = reg_model.predict(data)
     # create JSON object
     output = {'prediction': prediction}
 
-    #return output
+    # return output
     return '''<h1>features passed as parameters are: {}</h1>
               <h1>The predicted value is: {}'''.format(data, output)
 
